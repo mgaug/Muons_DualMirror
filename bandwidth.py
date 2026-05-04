972674fda6f6addd6f34a84a8ad1b1753a102b3a
Markus Gaug <markusgaug@gmail.com>
2026-05-04 08:33:53 +0200

    allow for reduction of verbosivity

--- bandwidth.py
+++ bandwidth.py
@@ -14,7 +14,8 @@ from setup import has_display
 plot_contributions = True
 plot_xidet         = True
 plot_zenith        = True
-full_accuracy      = True
+full_accuracy      = False     # fully accurate simulations, but very slow!
+verbose_output     = False     # more information about all used parameters
 
 # general configuration
 theta_tel = 0.    # default telescope pointing zenith angle, in degrees
@@ -23,7 +24,7 @@ rhoR_min  = 0.1   # reference lower impact distance cut, in rhoR/R1
 scale_h   = 9500  # La Palma Winter reference molecular density scale height
 tel_height = 2200 # default telescope height asl in meters, SHOULD BE ADJUSTED FOR EACH TELESCOPE INDIVIDUALLY
 
-bh = BandwidthHelper() # TO MODIFY THE BANDWIDTH MEASUREMENTS of the optical elements, EDIT THE FILE bandwidth_helper_v.py
+bh = BandwidthHelper(verbose=verbose_output) # TO MODIFY THE BANDWIDTH MEASUREMENTS of the optical elements, EDIT THE FILE bandwidth_helper_v.py
 
 # FOR THE CALCULATIONS OF B_gamma, PLEASE CAREFULLY REVISE THE ATMOSPHERIC TRANSMISSION TABLE USED FOR atm_file
 # AND THE (WRONG) ASSUMPTIONS USED IN IT AND THEIR UNCERTAINTIES IN THE DATACLASS AtmFileConfig in atmosphere_helper_v.py
@@ -31,31 +32,128 @@ atm_file = "data/atm_trans_2147_1_10_0_0_2147.dat"
 atm_config = AtmFileConfig()
 
 # DEFAULT MUON MODEL IS A CLEAR NIGHT. TO SIMULATE ANYTHING, USE THE DEFAULT CONSTRUCTOR MuonModel() with the corresponding arguments, see muonlight_averager_v.py
-model = MuonModel.from_LSTN(bandwidth=bh, tel_height=tel_height, theta_tel_deg=theta_tel, theta_c_deg=theta_c, rhoR_min=rhoR_min, scale_h=scale_h, atm_file=atm_file, atm_cfg=atm_config)
-model.print_summary()
 
+# LST-N
+#========
+
+model = MuonModel.from_LSTN(bandwidth=bh, tel_height=tel_height, theta_tel_deg=theta_tel, theta_c_deg=theta_c, rhoR_min=rhoR_min, scale_h=scale_h, atm_file=atm_file, atm_cfg=atm_config)
+if verbose_output:
+    model.print_summary()
+
+print("\n\nLST-N:")    
 print("B_mu =", model.bandwidth_muon())
 print("B_gamma =", model.bandwidth_gamma())
-print("ratio   =", model.ratio_gamma_to_muon())
+print("B_gamma/B_mu   =", model.ratio_gamma_to_muon())
 
 n_mc = 200 if full_accuracy else 20
 unc = model.simulate_uncertainty(        
     UncertaintyConfig(), 
-    n_mc=n_mc, 
+    n_mc=n_mc, verbose = verbose_output,
     full_accuracy=full_accuracy    
 )
-print(unc["B_mu"])
-print(unc["B_gamma"])
-print(unc["ratio_gamma_to_muon"])
+print(f"B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["B_mu"])
+print(f"B_gamma simulation results for theta_tel={theta_tel:.0f}: ",unc["B_gamma"])
+print(f"B_gamma/B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["ratio_gamma_to_muon"])
+
+# MST-N
+#========
+
+model = MuonModel.from_MSTN(bandwidth=bh, tel_height=tel_height, theta_tel_deg=theta_tel, theta_c_deg=theta_c, rhoR_min=rhoR_min, scale_h=scale_h, atm_file=atm_file, atm_cfg=atm_config)
+if verbose_output:
+    model.print_summary()
+
+print("\n\nMST-N:")
+print("B_mu =", model.bandwidth_muon())
+print("B_gamma =", model.bandwidth_gamma())
+print("B_gamma/B_mu   =", model.ratio_gamma_to_muon())
+
+n_mc = 200 if full_accuracy else 20
+unc = model.simulate_uncertainty(        
+    UncertaintyConfig(), 
+    n_mc=n_mc, verbose = verbose_output,
+    full_accuracy=full_accuracy    
+)
+print(f"B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["B_mu"])
+print(f"B_gamma simulation results for theta_tel={theta_tel:.0f}: ",unc["B_gamma"])
+print(f"B_gamma/B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["ratio_gamma_to_muon"])
+
+# LST-S
+#========
+
+model = MuonModel.from_LSTS(bandwidth=bh, tel_height=tel_height, theta_tel_deg=theta_tel, theta_c_deg=theta_c, rhoR_min=rhoR_min, scale_h=scale_h, atm_file=atm_file, atm_cfg=atm_config)
+if verbose_output:
+    model.print_summary()
+
+print("\n\nLST-S:")    
+print("B_mu =", model.bandwidth_muon())
+print("B_gamma =", model.bandwidth_gamma())
+print("B_gamma/B_mu   =", model.ratio_gamma_to_muon())
+
+n_mc = 200 if full_accuracy else 20
+unc = model.simulate_uncertainty(        
+    UncertaintyConfig(), 
+    n_mc=n_mc, verbose = verbose_output,
+    full_accuracy=full_accuracy    
+)
+print(f"B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["B_mu"])
+print(f"B_gamma simulation results for theta_tel={theta_tel:.0f}: ",unc["B_gamma"])
+print(f"B_gamma/B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["ratio_gamma_to_muon"])
+
+# MST-S
+#========
+
+model = MuonModel.from_MSTS(bandwidth=bh, tel_height=tel_height, theta_tel_deg=theta_tel, theta_c_deg=theta_c, rhoR_min=rhoR_min, scale_h=scale_h, atm_file=atm_file, atm_cfg=atm_config)
+if verbose_output:
+    model.print_summary()
+
+print("\n\nMST-S:")    
+print("B_mu =", model.bandwidth_muon())
+print("B_gamma =", model.bandwidth_gamma())
+print("B_gamma/B_mu   =", model.ratio_gamma_to_muon())
+
+n_mc = 200 if full_accuracy else 20
+unc = model.simulate_uncertainty(        
+    UncertaintyConfig(), 
+    n_mc=n_mc, verbose = verbose_output,
+    full_accuracy=full_accuracy    
+)
+print(f"B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["B_mu"])
+print(f"B_gamma simulation results for theta_tel={theta_tel:.0f}: ",unc["B_gamma"])
+print(f"B_gamma/B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["ratio_gamma_to_muon"])
+
+# SST-S
+#========
+
+model = MuonModel.from_SSTS(bandwidth=bh, tel_height=tel_height, theta_tel_deg=theta_tel, theta_c_deg=theta_c, rhoR_min=rhoR_min, scale_h=scale_h, atm_file=atm_file, atm_cfg=atm_config)
+if verbose_output:
+    model.print_summary()
+
+print("\n\nSST-S:")    
+print("B_mu =", model.bandwidth_muon())
+print("B_gamma =", model.bandwidth_gamma())
+print("B_gamma/B_mu   =", model.ratio_gamma_to_muon())
+
+n_mc = 200 if full_accuracy else 30
+unc = model.simulate_uncertainty(        
+    UncertaintyConfig(), 
+    n_mc=n_mc, verbose = verbose_output,
+    full_accuracy=full_accuracy    
+)
+print(f"B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["B_mu"])
+print(f"B_gamma simulation results for theta_tel={theta_tel:.0f}: ",unc["B_gamma"])
+print(f"B_gamma/B_mu simulation results for theta_tel={theta_tel:.0f}: ",unc["ratio_gamma_to_muon"])
+
+#==========================================================================================================
+
+# B vs. zenith
 
 # BEWARE THIS PART IS VERY SLOW IF full_accuracy IS CHOSEN. IF YOU WANT TO SPEED UP,
 # PLEASE SET THIS FLAG TO FALSE (AT THE PRICE OF LOWER ACCURACY OF THE RESULTS)
 fig, ax = plt.subplots(3,2,constrained_layout=True,figsize=(10,10))
 MuonModel.plot_bandwidth_vs_zenith(
     filename="output/B_vs_zenith{s}.pdf".format(s="_full_accuracy" if full_accuracy is True else "_reduced_accuracy"),
-    ax=ax, uncertainties=True, full_accuracy=full_accuracy, verbose=True, 
+    ax=ax, uncertainties=True, full_accuracy=full_accuracy, verbose=verbose_output, show=has_display
 )
-plt.show()
 
 fig, ax = plt.subplots(3,1,constrained_layout=True,figsize=(20,15))
 MuonModel.plot_transmission_vs_zenith(
